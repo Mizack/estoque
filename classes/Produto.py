6b9e5485d5258604a248ce2ed36c74f262ba8409
mizack mianuti <mizack1234@gmail.com>
2022-06-15 21:55:44 -0300

    falta arrumar bug de lista de erros

--- classes/Produto.py
+++ classes/Produto.py
@@ -1,21 +1,26 @@
+import sys
+sys.path.append(".")
 from classes.Validacoes import Validacoes
 class Produto:
+    
     def __init__(self,nome:str,valor:float,descricao:str,quantidade:int,codigo:int=0):
-        self.codigo = codigo
         self.nome = nome
         self.valor = valor
         self.descricao = descricao
         self.quantidade = quantidade
+        if codigo != 0:
+            self.codigo = codigo
+        self.lista_erros = []
+        
+        self.validacoes = Validacoes()
 
-        self._lista_erros = []
-        self._validacoes = Validacoes()
-
-        if self._lista_erros != []:
-            erro = {
-                "codigo":405,
-                "data":self._lista_erros
-            }
-            return erro 
+        # if self.lista_erros != []:
+        #     erro = {
+        #         "codigo":405,
+        #         "data":self.lista_erros
+        #     }
+        #     print(erro)
+        #     return erro 
 
     @property
     def codigo(self):
@@ -26,8 +31,7 @@ class Produto:
         if self.__validar_campo_vazio(codigo):
             self._codigo = codigo
         else:
-            # self._lista_erros.append(self._validacoes.gerar_codigo_status(400,"Erro na validacao do codigo"))
-            print("Erro na validacao do codigo")
+            print("Erro na validação de codigo")
 
 
     @property
@@ -36,11 +40,12 @@ class Produto:
 
     @nome.setter
     def nome(self, nome):
-        if type(nome) == str and len(nome)<201 or self.__validar_campo_vazio(nome):
+        if self.__validar_campo_vazio(nome):
             self._nome = nome
         else:
-            # self._lista_erros.append(self._validacoes.gerar_codigo_status(400,"Erro na validacao do nome"))
-            print("Erro na validacao do nome")
+            # self.lista_erros("Erro na validação de nome")
+            # print(self.lista_erros)
+            print("Erro na validação de nome")
 
 
     @property
@@ -49,12 +54,10 @@ class Produto:
 
     @valor.setter
     def valor(self, valor):
-        # if type(valor) == float and not self.__validar_campo_vazio(valor):
         if self.__validar_campo_vazio(valor):
             self._valor = valor
         else:
-            # self._lista_erros.append(self._validacoes.gerar_codigo_status(400,"Erro na validacao do valor"))
-            print("Erro na validacao do valor")
+            print("Erro na validação de valor")
 
 
     @property
@@ -63,12 +66,10 @@ class Produto:
 
     @descricao.setter
     def descricao(self, descricao):
-        
-        if type(descricao) == str and len(descricao)<401 or self.__validar_campo_vazio(descricao):
+        if self.__validar_campo_vazio(descricao):
             self._descricao = descricao
         else:
-            # self._lista_erros.append(self._validacoes.gerar_codigo_status(400,"Erro na validacao da descrição"))
-            print("Erro na validacao da descrição")
+            print("Erro na validação de descricao")
 
 
     @property
@@ -77,27 +78,33 @@ class Produto:
 
     @quantidade.setter
     def quantidade(self, quantidade):
-        # if type(quantidade) == int and not self.__validar_campo_vazio(quantidade):
         if self.__validar_campo_vazio(quantidade):
             self._quantidade = quantidade
         else:
-            # self._lista_erros.append(self._validacoes.gerar_codigo_status(400,"Erro na validacao da quantidade"))
-            print("Erro na validacao da quantidade")
+            print("Erro na validação de quantidade")
+
+
+    @property
+    def lista_erros(self):
+        return self._lista_erros
+
+    @lista_erros.setter
+    def lista_erros(self,lista_erros):
+        print('acessou')
+        lista = ["a"]
+        self._lista_erros = lista.append(lista_erros)
+        print(self._lista_erros)
 
     def retornar_dados_insercao(self)->list:
         dados = (self.nome,self.valor,self.descricao,self.quantidade)
         return [dados]
     
     def retornar_dados_alterar(self)->list:
-        print(self.nome)
         dados = (self.nome,self.valor,self.descricao,self.quantidade,self.codigo)
         return [dados]
 
     def __validar_campo_vazio(self,campo)->bool:
-        if campo == "" or campo == None:
+        if campo == "" or campo == None or not campo:
             return False
         else:
             return True
-
-    
-        
